5e264f9a09d52807caba568fb5931b837591d475
acoelho <coelho.amp@gmail.com>
2015-08-07 20:06:00 -0700

    new, improved

--- plot3.R
+++ plot3.R
@@ -1,3 +1,13 @@
+#download data if needed
+
+if (!file.exists("./household_power_consumption.txt")) {
+  if (!file.exists("./exdata%2Fdata%2Fhousehold_power_consumption.zip")) {
+    fURL <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
+    download.file(fURL,destfile="exdata%2Fdata%2Fhousehold_power_consumption.zip")
+  }
+  unzip("./exdata%2Fdata%2Fhousehold_power_consumption.zip")
+}
+
 #read the data
 data <- read.table("household_power_consumption.txt", sep = ";", header=T, na.strings="?")
 #subset the data for the deisred dates
